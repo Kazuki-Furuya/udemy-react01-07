ced5aa82d6e8a6693d6738abdf049393dbc32430
kazuki-furuya <furuyakazuki@gmail.com>
2023-02-05 00:30:15 +0900

    7-53

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,34 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div>
+        <input placeholder="input TODO" />
+        <button>add</button>
+      </div>
+      <div>
+        <p>incompleted todo</p>
+        <ul>
+          <div>
+            <li>aaa</li>
+            <button>complete</button>
+            <button>delete</button>
+          </div>
+          <div>
+            <li>bbb</li>
+            <button>complete</button>
+            <button>delete</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>completed todo</p>
+        <ul>
+          <li>ccc</li>
+          <button>back</button>
+        </ul>
+      </div>
+    </>
+  );
 };
