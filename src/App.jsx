5ca64b5c353db633a40f5fd3cf2f6af89a387c3a
kazuki-furuya <furuyakazuki@gmail.com>
2023-02-05 18:17:02 +0900

    7-55

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["aaa", "bbb"]);
   const [completeTodos, setCompleteTodos] = useState(["ccc"]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = (event) => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
 
   return (
     <>
       <div className="input-area">
-        <input placeholder="input TODO" />
-        <button>add</button>
+        <input
+          placeholder="input TODO"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>add</button>
       </div>
       <div className="incomplete-area">
         <p className="title">incompleted todo</p>
